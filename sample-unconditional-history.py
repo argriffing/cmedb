694feda77c678caefaf713b44b73c3eb7af833ab
alex <argriffi@ncsu.edu>
2013-03-15 15:14:54 -0400

    more work on unconditional sampling of substitution histories on a tree

--- sample-unconditional-history.py
+++ sample-unconditional-history.py
@@ -22,30 +22,112 @@ import sqlite3
 import numpy as np
 import networkx as nx
 
+def decompose_rates(Q):
+    """
+    Break a rate matrix into two parts.
+    The first part consists of the rates away from each state;
+    this information is contained in the diagonal of the rate matrix.
+    The second part consists of a transition matrix
+    that defines the distribution over sink states conditional
+    on an instantaneous change away from a given source state.
+    Note that this function never requires expm of Q.
+    Also, P preserves the sparsity pattern of Q.
+    @param Q: rate matrix
+    @return: rates, P
+    """
+    nstates = len(Q)
+    rates = -np.diag(Q)
+    P = np.array(Q)
+    for i, rate in enumerate(rates):
+        if rate:
+            P[i, i] = 0
+            P[i] /= rate
+    return rates, P
 
-def sample_history(root, G_dag_in, edge_to_blen_in, distn, Q):
+def gen_branch_history_sample(state_in, blen_in, rates, P):
     """
+    Path sampling along a branch with a known initial state.
+    Yield (transition time, new state) pairs.
+    The path sampling is conditional on the initial state
+    but it is not conditional on the final state.
+    So this is a 'forward' rather than a 'bridge' sampling.
+    It does not require any matrix exponential computation.
+    @param state_in: initial state
+    @param blen_in: length of the branch
+    @param rates: the rate away from each state
+    @param P: transition matrix conditional on leaving a state
+    """
+    state = state_in
+    nstates = len(rates)
+    blen_accum = 0
+    while True:
+        rate = rates[state]
+        scale = 1 / rate
+        b = np.random.exponential(scale=scale)
+        blen_accum += b
+        if blen_accum >= blen_in:
+            return
+        distn = P[state]
+        new_state = np.dot(np.arange(nstates), np.random.multinomial(1, distn))
+        yield blen_accum, new_state
+
+
+def sample_history(root, G_dag_in, distn, rates, P):
+    """
+    Sample state history on a rooted tree.
+    The input tree is a networkx directed acyclic graph
+    with 'blen' annotation on edges.
     @param root: root of the tree
-    @param G_dag_in: tree as a directed acyclic graph
-    @param edge_to_blen_in: branch length info
+    @param G_dag_in: tree as a networkx directed acyclic graph
     @param distn: state distribution at the root
-    @param 
-    @return: nx tree, map from edge to blen, map from edge to state
+    @param rates: the rate away from each state
+    @param P: transition matrix conditional on leaving a state
+    @return: networkx tree with edges annotated with 'blen' and 'state'
     """
-    #XXX unfinished
-    # populate the tree topology table
-    edge_va_vb_list = (
-            (0, 0, 5),
-            (1, 1, 5),
-            (2, 2, 6),
-            (3, 3, 6),
-            (4, 4, 7),
-            (5, 6, 7),
-            (6, 5, 7),
-            )
-    for edge_va_vb in edge_va_vb_list:
-        cursor.execute('insert into topo values (?, ?, ?)', edge_va_vb)
-    conn.commit()
+    nstates = len(distn)
+    if P.shape != (nstates, nstates):
+        raise Exception('nstates mismatch')
+    # Sample the initial state from the distribution.
+    # Python and numpy and scipy seem to not have categorial distribution
+    # (weighted choice) implementations, so I will use the multinomial
+    # sampling function instead.
+    v = np.random.multinomial(1, distn)
+    root_state = np.dot(np.arange(nstates), v)
+    # Initialize the root state.
+    vertex_to_state = {root : root_state}
+    # Note that the subset of vertices that are shared with the
+    # original unsegmented tree should have the same index
+    # numbers in the new segmented tree.
+    G = nx.Graph()
+    vertices = list(G_dag_in)
+    next_vertex = max(vertices) + 1
+    for node in nx.topological_sort(G_dag_in):
+        initial_state = vertex_to_state[node]
+        for successor in G_dag_in.successors(node):
+            blen = G_dag_in[node][successor]['blen']
+            accum = 0
+            segment_state = initial_state
+            v = node
+            for t, j in gen_branch_history_sample(root_state, blen, rates, P):
+                G.add_edge(
+                        v,
+                        next_vertex,
+                        blen=t-accum,
+                        state=segment_state,
+                        )
+                segment_state = j
+                v = next_vertex
+                next_vertex += 1
+                accum = t
+            G.add_edge(
+                    v,
+                    successor,
+                    blen=blen-accum,
+                    state=segment_state,
+                    )
+            vertex_to_state[successor] = segment_state
+    return G
+
 
 def main(args):
 
@@ -76,6 +158,12 @@ def main(args):
     for si, sj, rate in cursor:
         pre_Q[s_to_i[si], s_to_i[sj]] = rate
     Q = pre_Q - np.diag(np.sum(pre_Q, axis=1))
+    # construct the distribution of states at the root
+    cursor.execute('select state, prob from distn')
+    state_prob_pairs = list(cursor)
+    distn = np.zeros(nstates)
+    for state, prob in state_prob_pairs:
+        distn[s_to_i[state]] = prob
     conn.close()
 
     # open the tree db file
@@ -109,7 +197,7 @@ def main(args):
     # build an undirected graph from the tree info
     G = nx.Graph()
     for edge, va, vb in edge_va_vb_list:
-        G.add_edge(va, vb)
+        G.add_edge(va, vb, blen=edge_to_blen[edge])
 
     # check that the graph is connected and has no cycles
     if not nx.is_connected(G):
@@ -117,14 +205,18 @@ def main(args):
     if nx.cycle_basis(G):
         raise Exception('the tree has a cycle')
 
-    # build a directed breadth first tree starting at the distinguished vertex
+    # Build a directed breadth first tree starting at the distinguished vertex.
+    # Note that the tree built by nx.bfs_tree and the edges yielded
+    # by nx.bfs_edges do not retain the edge attributes.
     G_dag = nx.bfs_tree(G, root)
+    for a, b in G_dag.edges():
+        G_dag[a][b]['blen'] = G[a][b]['blen']
 
     # sample the unconditional histories
     conn = sqlite3.connect('histories.db')
     cursor = conn.cursor()
     cursor.execute(
-            'create table histories ('
+            'create table if not exists histories ('
             'history integer, '
             'segment integer, '
             'va integer, '
@@ -133,9 +225,14 @@ def main(args):
             'state integer, '
             'primary key (history, segment))')
     conn.commit()
+
+    # get the samples
+    print 'histories:'
+    rates, P = decompose_rates(Q)
     for i in range(nsamples):
-        pass
-        #XXX unfinished
+        G_segmented = sample_history(root, G_dag, distn, rates, P)
+        print nx.to_edgelist(G_segmented)
+        print
 
 
 if __name__ == '__main__':
@@ -147,7 +244,7 @@ if __name__ == '__main__':
                 'as an sqlite3 database file'))
     parser.add_argument('--root', type=int,
             help='root node index')
-    parser.add_argument('--nsamples', type=int, default=10,
+    parser.add_argument('--nsamples', type=int, default=5,
             help='sample this many histories')
     main(parser.parse_args())
 
