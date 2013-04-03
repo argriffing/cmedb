12684e4df32e10a0a0a7f09564933c7b08fa7141
alex <argriffi@ncsu.edu>
2013-04-02 22:20:00 -0400

    the construction of the muse-gaut codon rate matrix now seems to work

--- create-mg94.py
+++ create-mg94.py
@@ -16,30 +16,65 @@ import scipy.linalg
 import cmedbutil
 
 
-#XXX unfinished
 
-def get_MG_pre_Q(
-        ts, tv, syn, nonsyn, asym_compo,
-        nt_distn,
-        kappa, omega,
-        ):
-    """
-    This model is nested in FMutSel-F from which this code was copypasted.
-    It was found in slowedml/codon1994.py
-    """
-    if nt_distn.shape != (4,):
-        raise Exception(nt_distn.shape)
-    A = (omega * nonsyn + syn) * (kappa * ts + tv)
-    B = np.dot(asym_compo, nt_distn)
-    pre_Q = A * B
-    return pre_Q
+def hamming_distance(a, b):
+    return sum(1 for x, y in zip(a, b) if x != y)
 
 
 def main(args):
 
     # construct and validate the mutational process equilibrium distribution
-    ntdistn = np.array([args.A, args.C, args.G, args.T])
-    cmedbutil.assert_stochastic_vector(ntdistn)
+    nt_distn = np.array([args.A, args.C, args.G, args.T])
+    cmedbutil.assert_stochastic_vector(nt_distn)
+    nt_prob_map = {
+            'A' : args.A,
+            'C' : args.C,
+            'G' : args.G,
+            'T' : args.T,
+            }
+
+    # read the genetic code
+    conn = sqlite3.connect(args.code)
+    cursor = conn.cursor()
+    cursor.execute(
+        "select state, residue, codon from code "
+        "where residue <> 'Stop' "
+        "order by state")
+    genetic_code = list(cursor)
+    conn.close()
+
+    # construct the mg94 rate matrix
+    nstates = len(genetic_code)
+    transitions = ('AG', 'GA', 'CT', 'TC')
+    pre_Q = np.zeros((nstates, nstates), dtype=float)
+    for a, (state_a, residue_a, codon_a) in enumerate(genetic_code):
+        for b, (state_b, residue_b, codon_b) in enumerate(genetic_code):
+            if hamming_distance(codon_a, codon_b) != 1:
+                continue
+            for nta, ntb in zip(codon_a, codon_b):
+                if nta != ntb:
+                    rate = nt_prob_map[ntb]
+                    if nta + ntb in transitions:
+                        rate *= args.kappa
+            if residue_a != residue_b:
+                rate *= args.omega
+            pre_Q[a, b] = rate
+    Q = pre_Q - np.diag(np.sum(pre_Q, axis=1))
+    pre_distn = np.empty(nstates, dtype=float)
+    for i, (state, residue, codon) in enumerate(genetic_code):
+        pre_distn[i] = np.prod([nt_prob_map[nt] for nt in codon])
+    distn = pre_distn / np.sum(pre_distn)
+
+    # rescale the rate matrix to have one expected substitution per time unit
+    rates = -np.diag(Q)
+    expectation = np.dot(distn, rates)
+    Q = Q / expectation
+
+    # check time-reversible rate matrix invariants
+    cmedbutil.assert_stochastic_vector(distn)
+    cmedbutil.assert_rate_matrix(Q)
+    cmedbutil.assert_equilibrium(Q, distn)
+    cmedbutil.assert_detailed_balance(Q, distn)
 
     # create or open the rate matrix database for writing
     conn = sqlite3.connect(args.outfile)
@@ -60,33 +95,28 @@ def main(args):
             'primary key (state))')
     conn.commit()
 
-    # define the nucleotide states
-    nt_states = 'ACGT'
-
     # populate the rate matrix table
-    for i, si in enumerate(states):
-        for j, sj in enumerate(states):
-            if i == j:
+    for a, (state_a, residue_a, codon_a) in enumerate(genetic_code):
+        for b, (state_b, residue_b, codon_b) in enumerate(genetic_code):
+            if a == b:
                 rate = 0
-            elif si+sj in ('AG', 'GA', 'CT', 'TC'):
-                rate = args.kappa * distn[j]
             else:
-                rate = distn[j]
+                rate = Q[a, b]
             if rate > 0:
                 cursor.execute(
                         'insert into rates values (?, ?, ?)',
-                        (i, j, rate),
+                        (state_a, state_b, rate),
                         )
     conn.commit()
 
     # populate the states table
-    for pair in enumerate(states):
-        cursor.execute('insert into states values (?, ?)', pair)
+    for state, residue, codon in genetic_code:
+        cursor.execute('insert into states values (?, ?)', (state, codon))
     conn.commit()
 
     # populate the state distribution table
-    for pair in enumerate(distn):
-        cursor.execute('insert into distn values (?, ?)', pair)
+    for prob, (state, residue, codon) in zip(distn, genetic_code):
+        cursor.execute('insert into distn values (?, ?)', (state, prob))
     conn.commit()
 
     # close the database connection
@@ -111,7 +141,7 @@ if __name__ == '__main__':
             help='nonsynonymous/synonymous ratio')
     parser.add_argument('--code', default='universal.code.db',
             help='input genetic code in sqlite3 format')
-    parser.add_argument('--outfile', default='rate.matrix.db',
+    parser.add_argument('--outfile', default='codon.rate.matrix.db',
             help='output rate matrix in sqlite3 format')
     main(parser.parse_args())
 
